432a724b083f9300d06c5695a7b1aaf2edc7f21f
Sema <123366660+Semafftp@users.noreply.github.com>
2023-05-09 14:24:54 +0700

    Add files via upload

--- extensions.py
+++ extensions.py
@@ -38,4 +38,4 @@ class СurrencyConverter:
             raise ConvertionException(f'Не удалось обработать количество {amount} ')
         r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
         total_base = json.loads(r.content)[keys[base]]
-        return total_base
+        return total_base * amount
